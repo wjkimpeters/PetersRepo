96614b2ec16982c97aa6073ed0c43cb06f4187e3
william peters <wpeters@twcny.rr.com>
2015-07-18 11:21:13 -0400

    update to 50

--- HelloWorld/src/HelloWorld.cpp
+++ HelloWorld/src/HelloWorld.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	std::cout << "Hello Bill" << std::endl;
 	std::shared_ptr<int> lcIntSharedPtr(new int);
-	*lcIntSharedPtr = 10;
+	*lcIntSharedPtr = 50;
 	std::cout << "Value =" << *lcIntSharedPtr << std::endl;
 	return 0;
 
